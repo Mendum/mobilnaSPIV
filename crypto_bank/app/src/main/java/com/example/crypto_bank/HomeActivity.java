fa0153a99c356df7076ae6597b65981dbc31862c
cobiii <glogovsekgal@gmail.com>
2021-03-13 17:09:15 +0100

    Home page logic

--- crypto_bank/app/src/main/java/com/example/crypto_bank/HomeActivity.java
+++ crypto_bank/app/src/main/java/com/example/crypto_bank/HomeActivity.java
@@ -2,13 +2,51 @@ package com.example.crypto_bank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    TextView textBalance;
+    Button buttonSend,buttonTransakcije,buttonProfil;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        textBalance = (TextView) findViewById(R.id.textBalance);
+        textBalance.setText("100BTC");
+
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(), SendActivity.class);
+                startActivity(i);
+            }
+        });
+
+        buttonTransakcije.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(), TransactionActivity.class);
+                startActivity(i);
+            }
+        });
+
+        buttonProfil.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(), ProfileActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
