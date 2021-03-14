fd6cb793c17ffb85a216cbe616366217db5ed5f8
DanielKvar <daniel.kvar@gmail.com>
2021-03-14 10:33:16 +0100

    Profil, pošiljanje Bitcoina.

--- crypto_bank/app/src/main/java/com/example/crypto_bank/ProfileActivity.java
+++ crypto_bank/app/src/main/java/com/example/crypto_bank/ProfileActivity.java
@@ -2,13 +2,33 @@ package com.example.crypto_bank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    TextView name;
+    TextView email;
+    TextView phone;
+    TextView taxID;
+    TextView birthdate;
+    SharedPreferences SP;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        SP = getApplicationContext().getSharedPreferences("preferences", MODE_PRIVATE);
+        name = (TextView)findViewById(R.id.nameText);
+        email = (TextView)findViewById(R.id.emailText);
+        phone = (TextView)findViewById(R.id.phoneText);
+        taxID = (TextView)findViewById(R.id.taxIdText);
+        birthdate = (TextView)findViewById(R.id.birthDateText);
+        name.setText(SP.getString("name", ""));
+        email.setText(SP.getString("email", ""));
+        phone.setText(SP.getString("telNum", ""));
+        taxID.setText(SP.getString("taxID", ""));
+        birthdate.setText(SP.getString("birthDate", ""));
     }
 }
--- crypto_bank/app/src/main/java/com/example/crypto_bank/SendActivity.java
+++ crypto_bank/app/src/main/java/com/example/crypto_bank/SendActivity.java
@@ -3,20 +3,25 @@ package com.example.crypto_bank;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SendActivity extends AppCompatActivity {
 
     EditText textName,textAmount;
     Button buttonSend;
+    SharedPreferences SP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send);
+        SP = getApplicationContext().getSharedPreferences("preferences", MODE_PRIVATE);
+        final SharedPreferences.Editor SPEditor = SP.edit();
         textName = (EditText) findViewById(R.id.textName);
         textAmount = (EditText) findViewById(R.id.textAmount);
         buttonSend= (Button)findViewById(R.id.buttonSend);
@@ -27,7 +32,14 @@ public class SendActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 Intent i = new Intent(getBaseContext(), HomeActivity.class);
-                i.putExtra("amount",textAmount.getText().toString());
+                Integer amount = Integer.parseInt(textAmount.getText().toString());
+                Integer balance = Integer.parseInt(SP.getString("Bitcoin", "0"));
+                if(amount > balance){
+                    Toast.makeText(getApplicationContext(), "Not enough balance!", Toast.LENGTH_SHORT).show();
+                }else{
+                    SPEditor.putString("Bitcoin", String.valueOf(balance - amount));
+                    SPEditor.apply();
+                }
                 startActivity(i);
             }
         });
